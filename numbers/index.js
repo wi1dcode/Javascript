5089f74783b14f10e48f79f438357af32422a92e
wi1dcode <magosdk@icloud.com>
2022-06-20 17:48:28 +0200

    Numbers

---
+++ numbers/index.js
@@ -0,0 +1,43 @@
+// 01 - Number
+
+const integer = 102
+const float = 13.9
+console.log(integer, float)
+
+// 02 - Convert
+
+const basic = 34
+const stringified = basic.toString()
+console.log(typeof stringified)
+
+// 03 - Round
+
+const num = 1.5
+const rounded = Math.round(num)
+console.log(rounded)
+
+// 04 - Arithmétique
+
+const test = 12
+const bis = 5
+console.log(test + bis)
+console.log(test - bis)
+console.log(test * bis)
+console.log(test / bis)
+console.log(test ** bis)
+
+
+// 05 - Reste division euclidienne
+
+console.log(999 % 1)
+// 999 devise par 1 et 3
+console.log(25 % 5)
+// 25 devise par 1 et 5
+console.log(190280 % 5)
+// 190280 devise par 1 2 4 5
+console.log(47 % 1)
+// 47 devise par 1
+console.log(691 % 5)
+// 691 devise par 1
+
+
