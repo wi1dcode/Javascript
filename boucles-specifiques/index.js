ea29f5acf7e833c1fde14972f17be50cb5c20716
wi1dcode <magosdk@icloud.com>
2022-06-28 14:58:31 +0200

    exc 3 done

--- boucles-specifiques/index.js
+++ boucles-specifiques/index.js
@@ -1,30 +1,45 @@
-// 01 - Map Double
+//  ********************** 01 - Map Double **********************
 
-const array = [1, 2, 3, 4, 5];
+// const array = [1, 2, 3, 4, 5];
 
-const double = array.map((array) => {
-    return array * 2
-})
+// const double = array.map((array) => {
+//     return array * 2
+// })
 
-console.log(double);
+// console.log(double);
 
-// 02 - Map Names
+// **********************  02 - Map Names **********************
 
-const longNames = [
-	{
-		firstName: "Jane",
-		lastName: "Doe"
-	},
-	{
-		firstName: "John",
-		lastName: "Smith"
-	}
-]
+// const longNames = [
+// 	{
+// 		firstName: "Jane",
+// 		lastName: "Doe"
+// 	},
+// 	{
+// 		firstName: "John",
+// 		lastName: "Smith"
+// 	}
+// ]
 
-const shortNames = longNames.map((longNames) => {
-    const name = `name: ${longNames.firstName} ${longNames.lastName}`
-    return name
+// const shortNames = longNames.map((longNames) => {
+//     const name = `name: ${longNames.firstName} ${longNames.lastName}`
+//     return name
     
+// })
+
+// console.log(shortNames);
+
+
+// ********************** 03 - Filter Numbers **********************
+
+const array = [1, "toto", 34, "javascript", 8] 
+
+const numbers = array.filter((array) => {
+    if (typeof(array) === "number") {
+        return array
+    }
 })
 
-console.log(shortNames);
+console.log(numbers);
+
+// ********************** 04 - Filter Even **********************
